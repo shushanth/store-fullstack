3f94276226bae1712f399b7ff6185a942e860e14
shushanth <shushant3112@gmail.com>
2024-06-25 14:28:30 +0200

    pets api and services

--- src/module/pet/constants/pet.constants.ts
+++
@@ -1,9 +0,0 @@
-export const IMAGE_UPLOAD_SIZE: number = 2 * 1024 * 1024;
-export const IMAGE_UPLOAD_TYPES: string[] = [
-  'image/jpeg',
-  'image/png',
-  'image/gif',
-  'image/webp',
-];
-export const IMAGE_UPLOAD_TYPES_ERROR: string =
-  'upload image file should be of type jpg, webp, png, jpeg';
